26dd4b37f2bfdf38f2dc56ea3d72c30cce396cef
ciamac-da <ciamac.davoudi@gmail.com>
2021-11-28 13:16:19 +0100

    Verified by Marti4n v1.0.4 Dynamic Links

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,7 +1,9 @@
   <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id">
+  <div v-for="job in jobs" :key="job.id" class="job">
+  <router-link :to='{ name: "JobDetails", params: { id: job.id }}'>
     <h2>{{ job.title }}</h2>
+  </router-link>
   </div>
 </template>
 
@@ -11,9 +13,9 @@ export default {
     return {
       jobs: [
         { title: 'Cia AEM Developer', id: 1, details: 'lorem1' },
-        { title: 'Cia MERN-STACK Developer', id: 1, details: 'lorem1' },
-        { title: 'Cia Web Developer', id: 2, details: 'lorem2' },
-        { title: 'Cia Vue Developer', id: 3, details: 'lorem3' },
+        { title: 'Cia MERN-STACK Developer', id: 2, details: 'lorem1' },
+        { title: 'Cia Web Developer', id: 3, details: 'lorem2' },
+        { title: 'Cia Vue Developer', id: 4, details: 'lorem3' },
       ],
     }
   }
@@ -21,4 +23,19 @@ export default {
 </script>
 
 <style>
+.job h2 {
+    background: "#333";
+    padding: 20px;
+    border-radius: 10px;
+    margin: 10px auto;
+    color: gray;
+    cursor: pointer;
+    max-width: 600px;
+}
+.job h2:hover {
+    background: #ddd;
+}
+.job a {
+    text-decoration: none;
+}
 </style>
